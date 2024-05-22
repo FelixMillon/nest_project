86205590150c12ee9f949af05c83f5dac9391cfa
iangalmiche4 <galmicheian@gmail.com>
2024-05-22 14:46:29 +0200

    adding deleteUser

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,5 @@
 
-import { Controller, Get, Post, Put, Delete, Param, Body, BadRequestException, Res  } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, BadRequestException, Res, HttpStatus } from '@nestjs/common';
 import { Response } from 'express';
 import { UserService } from './user.service';
 import { FormatResponse } from '../types/response';
@@ -8,9 +8,9 @@ import { User } from '../types/user';
 export class UserController {
     constructor(
         private readonly userService: UserService
-    ) {}
+    ) { }
     @Post()
-    async addUser(@Body() body: User, @Res() res: Response): Promise<Response>{
+    async addUser(@Body() body: User, @Res() res: Response): Promise<Response> {
         const email = body.email
         if (!this.isValidEmail(email)) {
             return res.status(400).json({
@@ -25,7 +25,7 @@ export class UserController {
         return res.status(201).json(userAdded);
     }
     @Put()
-    async updateUser(@Body() body: User, @Res() res: Response): Promise<Response>{
+    async updateUser(@Body() body: User, @Res() res: Response): Promise<Response> {
         const userUpdated = await this.userService.updateUser(
             body.email,
             body.password
@@ -33,18 +33,29 @@ export class UserController {
         return res.status(200).json(userUpdated);
     }
     @Get(':userId')
-    async getUser(@Param('userId') userId: string, @Res() res: Response): Promise<Response>{
+    async getUser(@Param('userId') userId: string, @Res() res: Response): Promise<Response> {
 
         const userGetted = await this.userService.getUser(
             userId
         );
         return res.status(200).json(userGetted);
     }
-
+    @Delete(':email')
+    async deleteUser(@Param('email') email: string, @Res() res: Response): Promise<Response> {
+        try {
+            const deletedUser = await this.userService.deleteUser(email);
+            if (deletedUser) {
+                return res.status(HttpStatus.OK).json({ message: 'User deleted successfully' });
+            } else {
+                return res.status(HttpStatus.NOT_FOUND).json({ message: 'User not found' });
+            }
+        } catch (error) {
+            return res.status(HttpStatus.BAD_REQUEST).json({ message: error.message });
+        }
+    }
     private isValidEmail(email) {
         const regex = /^(?:[a-zA-Z0-9_'^&/+-])+(?:\.[a-zA-Z0-9_'^&/+-]+)*@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$/;
         return regex.test(email);
-      }
-    
+    }
 }
 
